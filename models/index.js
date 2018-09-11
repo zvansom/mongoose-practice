95c435ceb67e99da89e45c576cf94fb61fc2c3ff
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-09-10 18:04:15 -0700

    Mongoose connection established.  Models exported.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 // Require mongoose node module
 const mongoose = require('mongoose');
 
-// TODO: Connect to Mongo database
+// Connect to Mongo database
+mongoose.connect(process.env.MONGOOSE_URL || 'mongodb://localhost:27017/hellowdi20', { useNewUrlParser: true });
 
-// TODO: Require your other models, and export them
+// Require your other models, and export them
+module.exports.Museum = require('./museum');
+module.exports.Piece = require('./piece')
 
 // NOTE: Your files can have multiple module.exports statements!
 // Make sure to export both your Museum and Piece models!
